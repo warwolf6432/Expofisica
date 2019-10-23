b144886a669c4d2d19fe84e37717558f2fd34f81
María Zapata <maria2001@hotmail.com>
2019-10-23 16:26:41 -0500

    splash

--- p5/app/src/main/java/com/example/p5/Splash.java
+++ p5/app/src/main/java/com/example/p5/Splash.java
@@ -2,7 +2,9 @@ package com.example.p5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            public void run()
+            {
+                Intent intent= new Intent (Splash.this, Bienvenida.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
