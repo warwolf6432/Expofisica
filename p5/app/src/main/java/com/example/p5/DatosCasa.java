ff49f28d5e9dc25ea0f18afef3a913561dba8b56
María Zapata <maria2001@hotmail.com>
2019-10-25 00:24:40 -0500

    datos

--- p5/app/src/main/java/com/example/p5/DatosCasa.java
+++ p5/app/src/main/java/com/example/p5/DatosCasa.java
@@ -35,7 +35,7 @@ public class DatosCasa extends AppCompatActivity
     private BluetoothAdapter btAdapter = null;
     private BluetoothSocket btSocket = null;
     private StringBuilder DataStringIN = new StringBuilder();
-    private DatosCasa.ConnectedThread MyConexionBT;
+    //private DatosCasa.ConnectedThread MyConexionBT;
     // Identificador unico de servicio - SPP UUID
     private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     // String para la direccion MAC
@@ -52,151 +52,8 @@ public class DatosCasa extends AppCompatActivity
 
         txtTemp=(TextView) findViewById(R.id.txtTemp);
         txtHumedad=(TextView) findViewById(R.id.txtHumedad);
-
-        //Aquí llega la información
-        bluetoothIn = new Handler()
-        {
-            public void handleMessage(android.os.Message msg)
-            {
-                if (msg.what == handlerState)
-                {
-                    String readMessage = (String) msg.obj;
-                    DataStringIN.append(readMessage);
-
-                    int endOfLineIndex = DataStringIN.indexOf("#");
-                    int endHumedad= DataStringIN.indexOf("/");
-
-                    if (endOfLineIndex > 0)
-                    {
-                        //Variable que recibe los datos de la temperatura
-                        String dataInPrint = DataStringIN.substring(0, endOfLineIndex);
-                        txtTemp.setText("Temperatura: " + dataInPrint);
-                        DataStringIN.delete(0, DataStringIN.length());
-                    }
-                    if(endHumedad>0)
-                    {
-                        //Variable que recibe los datos de la humedad
-                        String dataHum= DataStringIN.substring(0, endHumedad);
-                        txtHumedad.setText("Humedad:" + dataHum);
-                    }
-                }
-            }
-        };
     }
 
-    //Crear una conexión segura
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException
-    {
-        //crea un conexion de salida segura para el dispositivo
-        //usando el servicio UUID
-        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        //Consigue la direccion MAC desde DeviceListActivity via intent
-        Intent intent = getIntent();
-        //Consigue la direccion MAC desde DeviceListActivity via EXTRA
-        address = intent.getStringExtra(DispositivosBT.EXTRA_DEVICE_ADDRESS);
-        //Setea la direccion MAC
-        BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-        try {
-            btSocket = createBluetoothSocket(device);
-        } catch (IOException e) {
-            Toast.makeText(getBaseContext(), "La creacción del Socket falló", Toast.LENGTH_LONG).show();
-        }
-        // Establece la conexión con el socket Bluetooth.
-        try {
-            btSocket.connect();
-        } catch (IOException e) {
-            try {
-                btSocket.close();
-            } catch (IOException e2) {
-            }
-        }
-        MyConexionBT = new DatosCasa.ConnectedThread(btSocket);
-        MyConexionBT.start();
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        try { // Cuando se sale de la aplicación esta parte permite
-            // que no se deje abierto el socket
-            btSocket.close();
-        } catch (IOException e2) {
-
-        }
-    }
-
-    //Comprueba que el dispositivo Bluetooth Bluetooth está disponible y solicita que se active si está desactivado
-    //Es decir, verifica si el dispositivo soporta o no soporta bluetooth.
-    private void VerificarEstadoBT()
-    {
-
-        if (btAdapter == null) {
-            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
-        } else {
-            if (btAdapter.isEnabled()) {
-            } else {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, 1);
-            }
-        }
-    }
-
-    //Crea la clase que permite crear el evento de conexion
-    private class ConnectedThread extends Thread
-    {
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-
-        public ConnectedThread(BluetoothSocket socket) {
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) {
-            }
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[256];
-            int bytes;
-
-            // Se mantiene en modo escucha para determinar el ingreso de datos
-            while (true) {
-                try {
-                    bytes = mmInStream.read(buffer);
-                    String readMessage = new String(buffer, 0, bytes);
-                    // Envia los datos obtenidos hacia el evento via handler
-                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-
-        //Envio de trama
-        public void write(String input)
-        {
-            try {
-                mmOutStream.write(input.getBytes());
-            } catch (IOException e) {
-                //si no es posible enviar datos se cierra la conexión
-                Toast.makeText(getBaseContext(), "La Conexión falló", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        }
-    }
 
     //MENÚ
     //Método para arrastras y ocultar el menú
